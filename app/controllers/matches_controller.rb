429dc4492d03213a8ecefb0e1e5625c2a0f1350e
Benjamin Liet <liet.ben@gmail.com>
2022-05-31 23:47:19 +0200

    Solidified code + little refacto

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,12 +1,39 @@
+require "json"
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class MatchesController < ApplicationController
   API_KEY = ENV['SPORTS_RADAR_API_KEY']
+  URL = "https://api.sportradar.com/tennis/trial/v3/en"
+  ENDPOINT = ".json?api_key=#{API_KEY}"
 
   def show
-    match_detail_url = "https://api.sportradar.com/tennis/trial/v3/en/sport_events/#{params[:id]}/summary.json?api_key=#{API_KEY}"
-    @match = JSON.parse(URI.open(match_detail_url).read)
+    player_id = "sr:sport_event:#{params[:id]}"
+    match_detail_url = "#{URL}/sport_events/#{player_id}/summary#{ENDPOINT}"
+    @match = search_for_data(match_detail_url)
+    sleep 1
     players = @match["sport_event"]["competitors"]
-    versus_detail_url = "https://api.sportradar.com/tennis/trial/v3/en/competitors/#{players[0]['id']}/versus/#{players[1]['id']}/summaries.json?api_key=#{API_KEY}"
-    @versus = JSON.parse(URI.open(versus_detail_url).read)
-    @key = API_KEY
+    @versus = define_versus(players[0]["id"], players[1]["id"])
+  end
+
+  private
+
+  def search_for_data(url)
+    url = URI(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+
+    response = http.request(request)
+    return JSON.parse(response.read_body)
+  end
+
+  def define_versus(player1, player2)
+    url = URI("https://api.sportradar.com/tennis/trial/v3/en/competitors/#{player1}/versus/#{player2}/summaries.json?api_key=#{API_KEY}")
+
+    return search_for_data(url)
   end
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,19 +1,39 @@
 require "json"
-require "open-uri"
+require 'uri'
+require 'net/http'
+require 'openssl'
 
 class PlayersController < ApplicationController
   API_KEY = ENV['SPORTS_RADAR_API_KEY']
-  URL = "https://api.sportradar.com/tennis/trial/v3/en/rankings.json?api_key=#{API_KEY}"
+  URL = "https://api.sportradar.com/tennis/trial/v3/en"
+  ENDPOINT = ".json?api_key=#{API_KEY}"
 
   def index
-    data = JSON.parse(URI.open(URL).read)
+    player_rankings_url = "#{URL}/rankings#{ENDPOINT}"
+    data = search_for_data(player_rankings_url)
     @players = data["rankings"][0]["competitor_rankings"]
   end
 
   def show
-    player_detail_url = "https://api.sportradar.com/tennis/trial/v3/en/competitors/#{params[:id]}/profile.json?api_key=#{API_KEY}"
-    @player = JSON.parse(URI.open(player_detail_url).read)
-    player_match_url = "https://api.sportradar.com/tennis/trial/v3/en/competitors/#{params[:id]}/summaries.json?api_key=#{API_KEY}"
-    @match = JSON.parse(URI.open(player_match_url).read)
+    player_id = "sr:competitor:#{params[:id]}"
+    player_detail_url = "#{URL}/competitors/#{player_id}/profile#{ENDPOINT}"
+    @player = search_for_data(player_detail_url)
+    sleep 1
+    player_matches_url = "#{URL}/competitors/#{player_id}/summaries#{ENDPOINT}"
+    @match = search_for_data(player_matches_url)
+  end
+
+  private
+
+  def search_for_data(url)
+    url = URI(url)
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+
+    response = http.request(request)
+    return JSON.parse(response.read_body)
   end
 end
