c50371b99d3184e7664d60f3424eba3cbd60027d
florian lequere <florian.lequere@gmail.com>
2022-05-31 16:38:10 +0200

    your players added to league dahsboard

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -16,7 +16,7 @@ class LeaguesController < ApplicationController
   def show
     @league = League.find(params[:id])
     @teams = @league.teams.sort_by { |element| -element[:points] }
-    @team = @league.teams.where(["user_id = ?", "current_user.id"])
+    @team = Team.where(["league_id = ? and user_id = ?", params[:id], current_user.id]).first
   end
 
   private
