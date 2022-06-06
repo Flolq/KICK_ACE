d4d48278c07af1b5bafa5e672b8103a510de3b97
pierre scheibling <pierre.scheibling@live.fr>
2022-06-06 14:31:30 +0200

    front, vh, grow

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -20,7 +20,7 @@ class LeaguesController < ApplicationController
 
   def show
     @league = League.find(params[:id])
-    @teams = @league.teams.sort_by { |element| -element[:points] }
+    @teams = @league.teams
     @team = Team.where(["league_id = ? and user_id = ?", params[:id], current_user.id]).first
     @selections = @team.selections.sort_by { |player| player[:position] }
   end
--- db/migrate/20220605080039_remove_points_from_selections.rb
+++
@@ -1,5 +0,0 @@
-class RemovePointsFromSelections < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :selections, :points, :integer
-  end
-end
