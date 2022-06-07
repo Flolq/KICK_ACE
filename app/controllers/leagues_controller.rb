6a482bbcb2c1923d6761e79b5d4510feff6efb72
Tai Nguyen <ngn.tai@gmail.com>
2022-06-07 18:35:37 +0200

    changing league#show to teams#bidding if the league is not complete

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -24,12 +24,19 @@ class LeaguesController < ApplicationController
 
   def show
     @league = League.find(params[:id])
-    @teams = @league.teams.order(points: :desc)
     @team = Team.where(["league_id = ? and user_id = ?", params[:id], current_user.id]).first
-    @selections = @team.selections.sort_by { |player| player[:position] }
-    @chatroom = Chatroom.where("league_id = ?", params[:id]).first
-    if !@chatroom.nil?
-      @message = @chatroom.messages.last
+    if @league.teams.length < @league.number_of_users
+      redirect_to submitted(@league, @team)
+      return
+    else
+      @league = League.find(params[:id])
+      @teams = @league.teams.order(points: :desc)
+      @team = Team.where(["league_id = ? and user_id = ?", params[:id], current_user.id]).first
+      @selections = @team.selections.sort_by { |player| player[:position] }
+      @chatroom = Chatroom.where("league_id = ?", params[:id]).first
+      if !@chatroom.nil?
+        @message = @chatroom.messages.last
+      end
     end
   end
 
