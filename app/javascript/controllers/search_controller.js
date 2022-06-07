1bf566f15bf187d1548d3e3a0419832daf8e4053
Tai Nguyen <ngn.tai@gmail.com>
2022-06-07 11:52:57 +0200

    starting fronting auctions

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,24 +1,24 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  // static targets = ["input","players","form"]
+  static targets = ["input","players","form"]
 
   connect() {
-    // console.log("hello from the search controller")
+    console.log("hello from the search controller")
     // console.log(this.inputTarget)
     // console.log(this.formTarget)
     // console.log(this.playersTarget)
   }
 
   update() {
-    console.log(this.formTarget.action)
-    console.log(this.inputTarget.value)
+    console.log(this.formTarget)
+    console.log(this.inputTarget)
     const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
       fetch(url, { headers: { "Accept": "text/plain" } })
         .then(response => response.text())
         .then((data) => {
-        this.playersTarget.innerHTML = data
-      })
+          this.playersTarget.innerHTML = data
+        })
   }
 
 }
