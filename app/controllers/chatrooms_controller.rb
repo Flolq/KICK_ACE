83661ec51bf31f2a74dc9e57c3be44c05bd197d3
Benjamin Liet <liet.ben@gmail.com>
2022-06-09 15:32:42 +0200

    Added navbar to chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+    @leagues = League.where(user_id: current_user)
+    @league = @chatroom.league
     @message = Message.new
   end
 end
